ee9564ff6444ed01008530bfdd3fc45f7cf0ce8f
rynusio <217859846+rynus-public@users.noreply.github.com>
2026-01-28 08:25:30 +0700

    fix build link

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/landing-page-update/', // 👈 TÊN REPO GITHUB (BẮT BUỘC)
+  base: '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
